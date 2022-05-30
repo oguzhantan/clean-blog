986b8ebf860cc7cd5cdc6027a72346c6b35035f8
oguzhantan <tannoguzhan97@gmail.com>
2022-05-30 22:29:23 +0300

    Part II

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 const express = require('express')
+const ejs = require('ejs')
+const path = require('path')
 
 const app = express();
 
+app.set("view engine","ejs");
+//Middleware
+app.use(express.static('public'));
+
 app.get("/",(req,res) =>{
-    const blog = { id: 1, title: "Blog title", description: "Blog description" }
-    res.send(blog)
+    res.render("index")
+    
+})
+
+app.get("/about",(req,res) =>{
+    res.render("about")
+    
+})
+
+app.get("/add_post",(req,res) =>{
+    res.render("add_post")
+    
 })
 
 const port = 3000;
