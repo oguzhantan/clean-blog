c8ad03946039900c8ef6b4467ebb7dd1bb4dee81
oguzhantan <tannoguzhan97@gmail.com>
2022-06-17 13:46:44 +0300

    Second

--- app.js
+++ app.js
@@ -1,17 +1,33 @@
 const express = require('express')
+const mongoose = require("mongoose");
+
 const ejs = require('ejs')
 const path = require('path')
 
+const Post = require('./model/Post')
+
 const app = express();
 
+//create DB
+mongoose.connect('mongodb://localhost/cleanblog-test-db',{
+    useNewUrlParser: true,
+      useUnifiedTopology: true,
+})
+
 app.set("view engine","ejs");
+
 //Middleware
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended:true }))
+app.use(express.json())
 
-app.get("/",(req,res) =>{
-    res.render("index")
-    
+app.get("/", async (req,res) =>{
+    const posts = await Post.find({})
+    res.render("index",{
+        posts
+    })
 })
+
 
 app.get("/about",(req,res) =>{
     res.render("about")
@@ -22,6 +38,15 @@ app.get("/add_post",(req,res) =>{
     res.render("add_post")
     
 })
+
+app.post("/post", async (req,res) => {
+    await Post.create(req.body)
+    res.redirect('/')
+})
+
+/*app.post('/post', (req,res) => {
+    console.log(req.body);
+})*/
 
 const port = 3000;
 app.listen(port,() => {
